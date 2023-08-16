051390a47bdbf6bb56b1ebbd5aea9e0292a46261
hong0314 <mi70107010@gmail.com>
2023-08-16 17:43:48 +0900

    .

--- independent_job/matrix/alg.py
+++ independent_job/matrix/alg.py
@@ -1,7 +1,6 @@
 class MatrixAlgorithm:
     def __init__(self, agent):
         self.agent = agent
-        self.skip = agent.skip
         
     def __call__(self, state):
         machine_feature = state.machine_feature
@@ -11,15 +10,14 @@ class MatrixAlgorithm:
         available_task_idx = state.available_task_idx
         task_num = task_feature.size(1)
 
-        if self.skip:
+        if self.agent.skip:
             task_selected = self.agent.decision(machine_feature, task_feature, D_TM, ninf_mask)
-            if task_selected == 0:
+            machine_pointer = task_selected // (task_num+1)
+            task_pointer = task_selected % (task_num+1)
+            if task_pointer == 0:
                 return None, None
             else:
-                task_selected -= 1
-                machine_pointer = task_selected // task_num
-                task_pointer = task_selected % task_num
-                return machine_pointer, available_task_idx[int(task_pointer)].item()
+                return machine_pointer, available_task_idx[int(task_pointer)-1].item()
 
         else: 
             task_selected = self.agent.decision(machine_feature, task_feature, D_TM, ninf_mask)
